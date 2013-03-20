2c5bf96fc2a3c32c9993a9431887377c9b1ef21b
David Whitney <david@davidwhitney.co.uk>
2013-03-20 11:38:14 +0000

    adding latest ndesk and switching to nuget.

---
+++ create-package.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "TransformVsConfiguration.sln"
+.nuget\nuget pack "TransformVsConfiguration\TransformVsConfiguration.csproj" -Properties Configuration=Release
